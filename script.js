0f211c04b7badb86138c5e598967986f1bdd776a
Etienne Detoisien <detoisienetienne@gmail.com>
2023-11-23 20:32:04 +0100

    Ajout d'images d'acteur et section offre

---
+++ script.js
@@ -0,0 +1,10 @@
+// const filmItem = document.querySelector(".filmItem")
+// const nameFilm = document.querySelectorAll(".filmItem .name")
+
+// filmItem.addEventListener("mouseenter", ()=> {
+//     console.log(nameFilm)
+//     nameFilm.classList.add("active")
+// })
+// filmItem.addEventListener("mouseleave", ()=> {
+//     nameFilm.classList.remove("active")
+// // })
